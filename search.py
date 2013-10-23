002e3513c37201b02234cc47df01be0f22281b3c
Michael Dickens <mtgandp@gmail.com>
2013-10-23 16:55:20 -0700

    Added PropertySearch class.

--- search.py
+++ search.py
@@ -10,24 +10,43 @@ Perform searches over a database.
 class SearchSchema():
 
     def __init__(self, courses):
+        # TODO: sanitize course names in self.courses
         self.courses = courses
 
-    def search(self, query):
+    def sanitizeQuery(self, query):
+        return query
+        # TODO: won't work because self.courses is not sanitized yet
+        # return query.strip().upper()
+
+    def searchSanitized(self, query):
         """
         Find courses that match `query`.
 
         Subclasses should implement this method.
+
+        query: Sanitized input string.
         """
-        raise Exception("not implemented")
+        raise Exception("Sub-classes should implement this method.")
+
+    def search(self, query):
+        return self.searchSanitized(self.sanitizeQuery(query))
 
         
-class SimpleSearch(SearchSchema):
+class NameSearch(SearchSchema):
     """
-    Search simply by finding keywords in the course list.
+    Search simply by finding the course with the given name.
     """
-    def search(self, query):
-        # Right now, if you input a course name, it returns the course data.
-        for k in courses:
+    def searchSanitized(self, query):
+        for k in self.courses:
             if query == k:
-                return courses[k]
+                return k # kind of useless
         return None
+
+class PropertySearch(SearchSchema):
+    """
+    Search for courses with the correct property.
+    """
+    def searchSanitized(self, query):
+        property, requirement = query.split()
+        return [ k for k in self.courses \
+                 if requirement in self.courses[k][property] ]
