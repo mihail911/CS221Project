94a7ceaa61f9e93d096cd65bef5d2f661c704279
Mihail Eric <meric@Mihail.(none)>
2013-11-02 13:38:30 -0700

    Got instructor names

--- CourseInformation.java
+++ CourseInformation.java
@@ -15,6 +15,7 @@ import edu.stanford.services.explorecourses.MeetingSchedule;
 import java.io.PrintWriter;
 import java.io.File;
 import java.io.*;
+import java.util.*;
 
 /** Prints out information for all courses offered at Stanford in an Academic Year **/
 public class CourseInformation
@@ -26,8 +27,20 @@ public class CourseInformation
     for(School s : connection.getSchools())
       for(Department d : s.getDepartments())
         for(Course c : connection.getCoursesByQuery(d.getCode())){
+          Set<String> allinstructors=new HashSet<String>();
+          String instructors="";
+          for (Section section: c.getSections()){
+            for(MeetingSchedule schedule : section.getMeetingSchedules()){
+                for(Instructor inst: schedule.getInstructors()){
+                    allinstructors.add(inst.getFirstName()+" "+inst.getLastName());
+                }
+            }
+          }
+          Object[] allinst=allinstructors.toArray();
+          for (int i=0; i<allinst.length; i++)
+            instructors=instructors+ allinst[i].toString()+" ";
         	writer.println("Course Title: "+c.getTitle());//get title of course
-        	writer.println("Course Instructors: "); //to be filled in here
+        	writer.println("Course Instructors: "+ instructors); //to be filled in here
         	writer.println("Course Units: "+Integer.toString(c.getMinimumUnits())+"-"+Integer.toString(c.getMaximumUnits()));
         	writer.println("Course Description: "+c.getDescription());
         	writer.println("----------------------------");	
