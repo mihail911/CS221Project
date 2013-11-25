e2ef9cbca7cc269156bbb2e0a8f7c1592231bd6c
Michael Dickens <mdickens93@gmail.com>
2013-11-25 15:20:08 -0600

    Added TODO.org and readDB.py

---
+++ readDB.py
@@ -0,0 +1,29 @@
+"""
+
+readDB.py
+---------
+
+Author: Michael Dickens, Mihail Eric
+Created: 2013-11-25
+
+Utilities to read the course database and provide useful output.
+
+""" 
+
+import sqlite3
+
+allcoursesdbname = "courseinfodata.db"
+instructordbname = "instructordatabase.db"
+
+allcoursesdb = sqlite3.connect(allcoursesdbname)
+
+def queryDB(query):
+    return allcoursesdb.execute(query)
+
+def cleanup():
+    """
+    Call this before exiting the program.
+    """
+    allcoursesdb.close()
+    instructordb.close()
+    
