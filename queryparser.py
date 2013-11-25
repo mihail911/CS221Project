9ee5db255b1229802a445815c8ff48e976afadbe
Mihail Eric <meric@Mihail.(none)>
2013-11-25 12:25:46 -0800

    updated readme

--- queryparser.py
+++ queryparser.py
@@ -1,9 +1,10 @@
+#!/usr/bin/python
 import nltk, re, pprint
 
 partsofspeechmap={'NNP': 'Proper Noun', 'NNS': 'Plural Noun', 'IN': 'Preposition', 'VB':'Verb Base Form', 'NN':'Singular noun', 'DT': 'Determiner'} #mapping from POS tag to actual meaning
 
 timeofday=['morning', 'afternoon', 'evening']
-quarters['autumn', 'winter', 'spring', 'summer']
+quarters=['autumn', 'winter', 'spring', 'summer']
 
 def parseQuery(query):
 	words=nltk.word_tokenize(query)
