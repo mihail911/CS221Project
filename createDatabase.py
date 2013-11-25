314be79f4adae45dd3cf2cccb54a0ee83be3d656
Mihail Eric <meric@Mihail.(none)>
2013-11-25 00:02:52 -0800

    new course info

--- createDatabase.py
+++ createDatabase.py
@@ -1,8 +1,10 @@
-#!usr/bin/Python
+#!/usr/bin/python
 """Provides a series of methods for creating a usable database from a text file
 of all available courses."""
 
-#f=open('courseinfo.txt','r')
+import sqlite3
+
+f=open('courseinfo2.txt','r')
 
 def oneClassInfo(f):
 	"""Reads in the contents of one file in Python and outputs information as a dictionary."""
@@ -10,17 +12,21 @@ def oneClassInfo(f):
 	courseinfo={}
 	coursetitle=' '.join(f.readline()[14:].split())
 	courseinstructors=' '.join(f.readline()[20:].split())
-	courseunits=' '.join(f.readline()[13:].split())
+	coursecode=' '.join(f.readline()[13:].split())	
+	courseunits=' '.join(f.readline()[13:].split())	
 	coursedescription=' '.join(f.readline()[20:].split())
 	delimiter=f.readline()
 
 	courseinfo['Course Title']=coursetitle
 	courseinfo['Course Instructors']=courseinstructors
 	courseinfo['Course Units']=courseunits
+	courseinfo['Course Code']=coursecode
 	courseinfo['Course Description']=coursedescription	
 	coursedict[coursetitle]=courseinfo
 	return coursedict
 
+dictexample=oneClassInfo(f)
+print dictexample
 ##not working yet!!
 def allClassInfo():
 	allcourses=[]
@@ -42,5 +48,12 @@ def allClassInfo():
 			allcourses.append(coursedict)	
 	return allcourses
 
-allcourses=allClassInfo()
-print allcourses
+def setupDatabase(allcourseinfo):
+	conn=sqlite3.connect('courseinfo_db.db')
+	curs=conn.cursor()
+	curs.execute('create table ALLCOURSES (title,code,instructor,units,description)')
+
+	conn.commit()
+	conn.close()
+#allcourses=allClassInfo()
+#print allcourses
