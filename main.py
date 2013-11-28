30b508623bf73753e0c4706f6f42e38a2a080c02
Mihail Eric <meric@Mihail.(none)>
2013-11-28 11:18:04 -0800

    added main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+"""
+main.py
+--------------
+
+Module we can use to run the entire program.
+
+"""
+import queryparser
+import createDatabase
+import readDB
+import coursegraph
+
+if __name__=='main':
+	"""
+	Run the program.
+	"""
