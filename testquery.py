698ebcbfe9e2e62feec17a80e8abafb3da9af5e6
Michael Dickens <mdickens93@gmail.com>
2013-12-12 22:05:46 -0800

    Working on query test.

--- testquery.py
+++ testquery.py
@@ -6,9 +6,20 @@ Test a series of queries using a point-based metric.
 
 """
 
-import queryparser
-import createDatabase
-import readDB
-import coursegraph
+from main import *
+from readDB import getField
 
-readDB.queryDB('')
+numresults = 3
+
+def top3(query):
+    return reversed([ entry for (entry, score) in processInput(query)[-3:] ])
+
+multipliers = [1.0, 0.6, 0.4]
+    
+def searchCode(code):
+    entries = top3(code)
+    points = 0
+    for i in range(numresults):
+        points +=  getField(entries[i], 'code') == code
+
+    
