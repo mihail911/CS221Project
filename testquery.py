8682b6a668bb796b743a4e92a86dd45da3fa6bb3
Michael Dickens <mdickens93@gmail.com>
2013-12-13 12:02:25 -0800

    Added our results to the Excel doc.

--- testquery.py
+++ testquery.py
@@ -38,39 +38,18 @@ leon simon
 amelang128b
 what is hemispheric
 first-year hausa
-first-year hebrew, first quarter (jewishst comparative fictions of ethnicity (amstud 51q, complit , csre51q )
+first-year hebrew, first quarter (jewishst 101a)
+comparative fictions of ethnicity (amstud 51q, complit , csre51q )
 amelang107b and csre14n
 growing up bilingual (chilatst 14n, educ math52h and cs105 )
 digital dilemmas
 amelang129a and cs109""".split("\n")
 
+queries = """math52h and cs105
+first-year hebrew, first quarter (jewishst 101a)
+comparative fictions of ethnicity (amstud 51q, complit , csre51q )
+""".split("\n")
+
 for query in queries:
     print "%s: %s\n" % (query, [ (getField(entry, 'code'), getField(entry, 'title'), getField(entry, 'instructor')) for entry in top3(query) ])
 
-
-"""
-
-Scores
-
-amelang110a: 100
-introduction to computing principles: 0
-amelang 144b: 60
-the mathematics of the rubik's cube: 0
-math120: 100
-third-year persian, second quarter: 0
-identity and popular music (femgen 140g, music , csre140g ): 100
-math53: 100
-courses taught by julie zelenski: 100
-courses taught by mehran sahami: 200
-leon simon: 100
-amelang128b: 100
-what is hemispheric: 0
-first-year hausa: 0
-first-year hebrew, first quarter (jewishst comparative fictions of ethnicity (amstud 51q, complit , csre51q ): 30
-amelang107b and csre14n: 100
-growing up bilingual (chilatst 14n, educ math52h and cs105 ): 0
-digital dilemmas: 100
-amelang129a and cs109: 100
-
-
-"""
